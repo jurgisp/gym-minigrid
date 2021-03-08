227a1fd79e2531ab256f49f7e7a7171e9d6c1772
Jurgis <jurgisp@gmail.com>
2021-03-08 17:30:44 +0200

    Add Maze environment

---
+++ gym_minigrid/envs/maze.py
@@ -0,0 +1,54 @@
+from gym_minigrid.minigrid import *
+from gym_minigrid.register import register
+
+
+class MazeEnv(MiniGridEnv):
+    """
+    Dense maze environment
+    """
+
+    def __init__(self,
+                 size=11,
+                 agent_start_pos=(1, 1),
+                 agent_start_dir=0,
+                 goal_pos=(-2, -2),
+                 max_steps=1000
+                 ):
+        assert size % 2 == 1, "Size should be odd"
+        self._agent_start_pos = agent_start_pos
+        self._agent_start_dir = agent_start_dir
+        self._goal_pos = goal_pos if goal_pos[0] >= 0 else (goal_pos[0] + size, goal_pos[1] + size)
+        super().__init__(grid_size=size, max_steps=max_steps)
+
+    def _gen_grid(self, width, height):
+        self.grid = Grid(width, height)
+
+        # Fill everything with walls
+        for i in range(width // 2 + 1):
+            self.grid.wall_rect(i, i, width - 2 * i, height - 2 * i)
+
+        visited = np.ones((width, height), dtype=bool)
+        visited[1:-1, 1:-1] = False
+
+        def _gen_maze_dfs(x, y):
+            self.grid.set(x, y, None)
+            visited[x, y] = True
+            moves = [(x - 2, y), (x + 2, y), (x, y - 2), (x, y + 2)]
+            moves = np.random.permutation(moves)
+            for tx, ty in moves:
+                if 0 <= tx < width and 0 <= ty < height and not visited[tx, ty]:
+                    self.grid.set((x + tx) // 2, (y + ty) // 2, None)  # set intermediate "corridor"
+                    _gen_maze_dfs(tx, ty)
+
+        _gen_maze_dfs(*self._agent_start_pos)
+        self.agent_pos = self._agent_start_pos
+        self.agent_dir = self._agent_start_dir
+        self.put_obj(Goal(), *self._goal_pos)
+
+        self.mission = "get to the green goal square"
+
+
+register(id='MiniGrid-Maze-v0', entry_point='gym_minigrid.envs:MazeEnv')
+register(id='MiniGrid-MazeS11-v0', entry_point='gym_minigrid.envs:MazeEnv', kwargs=dict(size=11))
+register(id='MiniGrid-MazeS15-v0', entry_point='gym_minigrid.envs:MazeEnv', kwargs=dict(size=15))
+register(id='MiniGrid-MazeS19-v0', entry_point='gym_minigrid.envs:MazeEnv', kwargs=dict(size=19))
